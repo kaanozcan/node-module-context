f4fbb57e0009dbd7a47cc6a2c03960e7f2153bb9
Kaan Özcan <knzcnn@gmail.com>
2017-07-20 18:13:20 +0300

    support node version 8.x (rc)

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -3,39 +3,56 @@ const path = require("path"),
   removeShebang
 } = require("../utils");
 
+//TODO: removed stat.cache check what it is doing
 module.exports = (Module, context, _require) => (function (content, filename) {
   const internalModule = _require("internal/module");
 
   content = removeShebang(content);
 
+  // create wrapper function
   var wrapper = Module.wrap(content);
+
+  if(!vm.isContext(context)){
+    vm.createContext(context);
+  }
+
   var compiledWrapper = vm.runInContext(wrapper, context, {
     filename: filename,
     lineOffset: 0,
     displayErrors: true
   });
 
-  if (process._debugWaitConnect && process._eval == null) {
+  var inspectorWrapper = null;
+  if (process._breakFirstLine && process._eval == null) {
     if (!resolvedArgv) {
+      // we enter the repl if we're not given a filename argument.
       if (process.argv[1]) {
-        resolvedArgv = Module._resolveFilename(process.argv[1], null);
+        resolvedArgv = Module._resolveFilename(process.argv[1], null, false);
       } else {
-        resolvedArgv = "repl";
+        resolvedArgv = 'repl';
       }
     }
 
     // Set breakpoint on module start
     if (filename === resolvedArgv) {
-      delete process._debugWaitConnect;
-      const Debug = vm.runInDebugContext("Debug");
-
-      Debug.setBreakPoint(compiledWrapper, 0, 0);
+      delete process._breakFirstLine;
+      inspectorWrapper = process.binding('inspector').callAndPauseOnStart;
+      if (!inspectorWrapper) {
+        const Debug = vm.runInDebugContext('Debug');
+        Debug.setBreakPoint(compiledWrapper, 0, 0);
+      }
     }
   }
   var dirname = path.dirname(filename);
-  var require = internalModule.makeRequireFunction.call(this);
-  var args = [this.exports, require, this, filename, dirname];
-  var result = compiledWrapper.apply(this.exports, args);
-
+  var require = internalModule.makeRequireFunction(this);
+  var depth = internalModule.requireDepth;
+  var result;
+  if (inspectorWrapper) {
+    result = inspectorWrapper(compiledWrapper, this.exports, this.exports,
+                              require, this, filename, dirname);
+  } else {
+    result = compiledWrapper.call(this.exports, this.exports, require, this,
+                                  filename, dirname);
+  }
   return result;
 });
