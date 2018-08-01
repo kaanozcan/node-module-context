a530737bc20d435a0d6ea7e29127ca8424510f32
Kaan Özcan <knzcnn@gmail.com>
2018-08-01 17:50:50 +0300

    added tls

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   extensionsIgnoredForPreload: [".json"],
-  nonInternalModules: ["module", "assert", "http", "https", "buffer", "fs", "path", "os", "tty", "util", "vm", "inherits", "zlib", "stream", "crypto", "child_process"],
-  coreModules: ["assert", "http", "https", "buffer", "fs", "path", "os", "tty", "util", "vm", "inherits", "zlib", "stream", "crypto"]
+  nonInternalModules: ["module", "assert", "http", "https", "buffer", "fs", "path", "os", "tty", "util", "vm", "inherits", "zlib", "stream", "crypto", "child_process", "tls"],
+  coreModules: ["assert", "http", "https", "buffer", "fs", "path", "os", "tty", "util", "vm", "inherits", "zlib", "stream", "crypto", "tls"]
 };
